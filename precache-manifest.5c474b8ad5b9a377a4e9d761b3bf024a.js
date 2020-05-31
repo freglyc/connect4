0ee887a429c9b9377d252a96405dd11ab0efb9e1
freglyc <cdf7@rice.edu>
2020-05-31 14:48:11 -0500

    Updates

---
+++ precache-manifest.5c474b8ad5b9a377a4e9d761b3bf024a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "252d04fac56d40811008816fc81da01b",
+    "url": "/connect4/index.html"
+  },
+  {
+    "revision": "d352eb0e8c84b4b662aa",
+    "url": "/connect4/static/css/main.c905d3c4.chunk.css"
+  },
+  {
+    "revision": "7c4395a1298c51dc18d6",
+    "url": "/connect4/static/js/2.8715251a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/connect4/static/js/2.8715251a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d352eb0e8c84b4b662aa",
+    "url": "/connect4/static/js/main.f6a42626.chunk.js"
+  },
+  {
+    "revision": "6dcd4889ffbf7da7aa15",
+    "url": "/connect4/static/js/runtime-main.885668b8.js"
+  },
+  {
+    "revision": "874514d651bf6c76840f78ea4f951d8d",
+    "url": "/connect4/static/media/blueFavicon.874514d6.ico"
+  },
+  {
+    "revision": "913600e4205320e13cfcba211b7ef7c7",
+    "url": "/connect4/static/media/defaultFavicon.913600e4.ico"
+  },
+  {
+    "revision": "bb6577c19981fc392d86f8b5f6f74801",
+    "url": "/connect4/static/media/greenFavicon.bb6577c1.ico"
+  },
+  {
+    "revision": "3401ab9d8931846ea1ed5fd374b09388",
+    "url": "/connect4/static/media/redFavicon.3401ab9d.ico"
+  },
+  {
+    "revision": "632fadd3ac1576e266b11bd787eaae1e",
+    "url": "/connect4/static/media/yellowFavicon.632fadd3.ico"
+  }
+]);
